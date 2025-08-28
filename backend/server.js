d71d40d00faeb9834a5d3286371f306c52b6ef86
ADAGA HAPPY <happyadaga@gmail.com>
2025-08-28 12:54:50 +0100

    first deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-import testRoutes from "./routes/testRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -19,7 +18,6 @@ connectDB();
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/test", testRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
